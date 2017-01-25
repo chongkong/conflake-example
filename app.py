a166a7bd81ac4f9578ea1cbe4109e0698790c295
박종빈 <chongkong94@gmail.com>
2017-01-25 17:48:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_env():
     return json.dumps({k: v for k, v in os.environ.items()}, indent=2)
+
+
+@app.route('/<path:path>')
+def serve(path):
+    return send_from_directory('.', path)
